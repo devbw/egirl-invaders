79aeb01f115702941416988f4887035a5d90dc1d
unknown <brian.wullaert@vts.team>
2023-12-29 19:12:20 +0100

    make obstacles moves

--- index.js
+++ index.js
@@ -5,43 +5,66 @@ const container = document.querySelector(".global");
 const waitscreen = document.querySelector(".waitscreen");
 
 let egirlDied = 0;
+let mouseX = container.clientWidth / 2;
+let vitess = 0.8;
 
 start.addEventListener("click", () => {
   waitscreen.style.display = "none";
-})
-
-let mouseX = container.clientWidth / 2;
+});
 
 document.addEventListener("click", () => createRocket());
-
-const randNum = () => {
-  return Math.floor(Math.random() * 2) + 1;
-}
-
-const createObstacle = (number) => {
-  for (let i = 0; i < number; i++) {
-    const choseImg = randNum();
-    const obstacle = document.createElement('img');
-    obstacle.className = 'obstacle';
-    obstacle.src = choseImg === 1 ? './img/egirl.png' : './img/egirl2.png';
-    obstacle.style.position = 'absolute';
-    obstacle.style.left = `${Math.floor(Math.random() * (container.clientWidth - 20))}px`;
-    obstacle.style.top = `${Math.floor(Math.random() * (container.clientHeight / 3))}px`;
-    container.appendChild(obstacle);
-  }
-}
-
-createObstacle(7)
 
 container.addEventListener("mousemove", (event) => {
   mouseX = event.clientX;
   moveCharacter();
 });
 
+const randNum = () => {
+  return Math.floor(Math.random() * 2) + 1;
+};
+
+const createObstacle = (number) => {
+  for (let i = 0; i < number; i++) {
+    const choseImg = randNum();
+    const obstacle = document.createElement("img");
+    obstacle.className = "obstacle no-outline";
+    obstacle.src = choseImg === 1 ? "./img/egirl.png" : "./img/egirl2.png";
+    obstacle.style.position = "absolute";
+    obstacle.style.left = `${Math.floor(Math.random() * (container.clientWidth - 20))}px`;
+    obstacle.style.top = `${Math.floor(Math.random() * (container.clientHeight / 3))}px`;
+
+    // Ajouter la propriété direction à l'obstacle (1 pour droite, -1 pour gauche)
+    obstacle.direction = Math.random() < 0.5 ? 1 : -1;
+
+    container.appendChild(obstacle);
+  }
+};
+
+createObstacle(7);
+
+const moveObstacles = () => {
+  const obstacles = document.querySelectorAll(".obstacle");
+
+  obstacles.forEach((obstacle) => {
+    const obstacleRect = obstacle.getBoundingClientRect();
+
+    obstacle.style.left = obstacleRect.left + obstacle.direction * vitess + "px";
+
+    if (obstacleRect.left > container.clientWidth && obstacle.direction === 1) {
+      obstacle.style.left = -10 + "px";
+    } else if (obstacleRect.right < -10 && obstacle.direction === -1) {
+      obstacle.style.left = container.clientWidth + "px";
+    }
+  });
+
+  requestAnimationFrame(moveObstacles);
+};
+
+moveObstacles();
+
 const moveCharacter = () => {
   character.style.left = mouseX + "px";
-  requestAnimationFrame(moveCharacter);
-}
+};
 
 const createRocket = () => {
   const rocket = document.createElement("div");
@@ -58,7 +81,7 @@ const createRocket = () => {
     if (newY >= 0) {
       rocket.style.top = newY + "px";
 
-      const obstacles = document.querySelectorAll('.obstacle');
+      const obstacles = document.querySelectorAll(".obstacle");
       obstacles.forEach((obstacle) => {
         const obstacleRect = obstacle.getBoundingClientRect();
 
@@ -75,8 +98,9 @@ const createRocket = () => {
           createObstacle(1);
         }
       });
-
-      requestAnimationFrame(moveRocket);
+      if (container.contains(rocket)) {
+        requestAnimationFrame(moveRocket);
+      }
     } else {
       container.removeChild(rocket);
     }
