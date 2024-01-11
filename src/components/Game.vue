967e27a900404a516d9bd4eca27d074789757371
devbw <wullaertbrian@gmail.com>
2024-01-11 17:32:58 +0100

    ^improve^ lose^ screen^

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -47,27 +47,49 @@
     </div>
 
     <div v-if="lose" class="losecontainer__lose">
-      <h2 class="losetext">Tu es un gros baiseur..</h2>
-      <div class="losecontainer__lose__score">
-        <div class="losecontainer__lose__score__bloc">
-          <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 20 20"><g fill="none"><path d="M17 2.5a.5.5 0 0 0-1 0V3h-.5a.5.5 0 0 0 0 1h.5v.5a.5.5 0 0 0 1 0V4h.5a.5.5 0 0 0 0-1H17v-.5zm-10 3a.5.5 0 0 1-.5.5H6v.5a.5.5 0 0 1-1 0V6h-.5a.5.5 0 0 1 0-1H5v-.5a.5.5 0 0 1 1 0V5h.5a.5.5 0 0 1 .5.5zm9 9a.5.5 0 0 1-.5.5H15v.5a.5.5 0 0 1-1 0V15h-.5a.5.5 0 0 1 0-1h.5v-.5a.5.5 0 0 1 1 0v.5h.5a.5.5 0 0 1 .5.5zm-2.565-7.934a1.914 1.914 0 0 0-2.708 0l-.477.477l2.707 2.707l.478-.477a1.914 1.914 0 0 0 0-2.707zm-1.185 3.891L9.543 7.75l-6.982 6.982a1.914 1.914 0 1 0 2.707 2.707l6.982-6.982z" fill="currentColor"></path></g></svg>
-          <p>Score : {{ enemiesKilled }}</p>
+      <div class="losebackground">
+        <h2 class="losetext">Tu es un gros baiseur..</h2>
+        <div class="losecontainer__lose__score">
+          <div class="losecontainer__lose__score__bloc">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              xmlns:xlink="http://www.w3.org/1999/xlink"
+              viewBox="0 0 20 20"
+            >
+              <g fill="none">
+                <path
+                  d="M17 2.5a.5.5 0 0 0-1 0V3h-.5a.5.5 0 0 0 0 1h.5v.5a.5.5 0 0 0 1 0V4h.5a.5.5 0 0 0 0-1H17v-.5zm-10 3a.5.5 0 0 1-.5.5H6v.5a.5.5 0 0 1-1 0V6h-.5a.5.5 0 0 1 0-1H5v-.5a.5.5 0 0 1 1 0V5h.5a.5.5 0 0 1 .5.5zm9 9a.5.5 0 0 1-.5.5H15v.5a.5.5 0 0 1-1 0V15h-.5a.5.5 0 0 1 0-1h.5v-.5a.5.5 0 0 1 1 0v.5h.5a.5.5 0 0 1 .5.5zm-2.565-7.934a1.914 1.914 0 0 0-2.708 0l-.477.477l2.707 2.707l.478-.477a1.914 1.914 0 0 0 0-2.707zm-1.185 3.891L9.543 7.75l-6.982 6.982a1.914 1.914 0 1 0 2.707 2.707l6.982-6.982z"
+                  fill="currentColor"
+                ></path>
+              </g>
+            </svg>
+            <p>Score : {{ enemiesKilled }}</p>
+          </div>
+          <div class="losecontainer__lose__score__bloc">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              xmlns:xlink="http://www.w3.org/1999/xlink"
+              viewBox="0 0 640 512"
+            >
+              <path
+                d="M528 448H112c-8.8 0-16 7.2-16 16v32c0 8.8 7.2 16 16 16h416c8.8 0 16-7.2 16-16v-32c0-8.8-7.2-16-16-16zm64-320c-26.5 0-48 21.5-48 48c0 7.1 1.6 13.7 4.4 19.8L476 239.2c-15.4 9.2-35.3 4-44.2-11.6L350.3 85C361 76.2 368 63 368 48c0-26.5-21.5-48-48-48s-48 21.5-48 48c0 15 7 28.2 17.7 37l-81.5 142.6c-8.9 15.6-28.9 20.8-44.2 11.6l-72.3-43.4c2.7-6 4.4-12.7 4.4-19.8c0-26.5-21.5-48-48-48S0 149.5 0 176s21.5 48 48 48c2.6 0 5.2-.4 7.7-.8L128 416h384l72.3-192.8c2.5.4 5.1.8 7.7.8c26.5 0 48-21.5 48-48s-21.5-48-48-48z"
+                fill="currentColor"
+              ></path>
+            </svg>
+            <p>lvl : {{ lvl }}</p>
+          </div>
+          <img
+            :src="usedSkin"
+            draggable="false"
+            class="losecontainer__lose__score__skin"
+          />
         </div>
-        <div class="losecontainer__lose__score__bloc">
-          <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 640 512"><path d="M528 448H112c-8.8 0-16 7.2-16 16v32c0 8.8 7.2 16 16 16h416c8.8 0 16-7.2 16-16v-32c0-8.8-7.2-16-16-16zm64-320c-26.5 0-48 21.5-48 48c0 7.1 1.6 13.7 4.4 19.8L476 239.2c-15.4 9.2-35.3 4-44.2-11.6L350.3 85C361 76.2 368 63 368 48c0-26.5-21.5-48-48-48s-48 21.5-48 48c0 15 7 28.2 17.7 37l-81.5 142.6c-8.9 15.6-28.9 20.8-44.2 11.6l-72.3-43.4c2.7-6 4.4-12.7 4.4-19.8c0-26.5-21.5-48-48-48S0 149.5 0 176s21.5 48 48 48c2.6 0 5.2-.4 7.7-.8L128 416h384l72.3-192.8c2.5.4 5.1.8 7.7.8c26.5 0 48-21.5 48-48s-21.5-48-48-48z" fill="currentColor"></path></svg>
-          <p>lvl : {{ lvl }}</p>
+        <div class="losecontainer__lose__thanks">
+          <p>
+            Merci pour leur aide et idées : KeroSan, Chapelain, Hunt, Honda,
+            Heavent, Esypso
+          </p>
         </div>
-        <img
-          :src="usedSkin"
-          draggable="false"
-          class="losecontainer__lose__score__skin"
-        />
-      </div>
-      <div class="losecontainer__lose__thanks">
-        <p>
-          Merci pour leur aide et idées : KeroSan, Chapelain, Hunt, Honda,
-          Heavent, Esypso
-        </p>
       </div>
     </div>
 
@@ -235,10 +257,11 @@ const type = async (isType, type) => {
 }
 
 .losecontainer__lose {
-  height: 50%;
-  background: white;
+  height: 55%;
+  background-color: rgb(10, 16, 51);
   box-shadow: 0px 10px 15px -3px rgba(0, 0, 0, 0.1);
   border-radius: 5px;
+  border: 10px solid white;
   position: absolute;
   top: 50%;
   left: 50%;
@@ -250,8 +273,17 @@ const type = async (isType, type) => {
   justify-content: space-between;
 }
 
+.losebackground {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  height: 100%;
+  background-image: url("https://i.goopics.net/whpw47.png");
+  background-size: auto 30px;
+}
+
 .losecontainer__lose h2 {
-  color: rgb(126, 16, 136);
+  color: rgb(255, 255, 255);
   margin-top: 0;
 }
 
@@ -289,6 +321,7 @@ const type = async (isType, type) => {
 
 .losecontainer__lose__score__skin {
   margin-top: 25px;
+  margin-bottom: 10px;
   width: 100px;
   height: auto;
 }
